786e6e439a66a59a49aa999e0f6fed36b5eee5ae
Mauro <mauro.mendozacp@gmail.com>
2022-10-01 01:44:27 -0300

    Implement player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 0f;
+    [SerializeField] private float jumpForce = 0f;
+    [SerializeField] private LayerMask jumpeableMask = default;
+
+    private Rigidbody rigid = null;
+    private float halfHeight = 0f;
+    private bool dead = false;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+
+        CapsuleCollider capsuleCollider = GetComponent<CapsuleCollider>();
+        halfHeight = capsuleCollider.height / 2 + 0.05f;
+    }
+
+    private void Update()
+    {
+        if (dead) return;
+
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        if (dead) return;
+
+        InputMovement();
+    }
+
+    private void InputMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            AddMovement(transform.forward);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            AddMovement(-transform.forward);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            AddMovement(transform.right);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            AddMovement(-transform.right);
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Physics.Raycast(transform.position, Vector3.down, halfHeight, jumpeableMask))
+            {
+                rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void AddMovement(Vector3 direction)
+    {
+        rigid.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.Acceleration);
+    }
+}

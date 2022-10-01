620e2863fd8f4ea5199909e5bf8f7195b6d9cc4e
Mauro <mauro.mendozacp@gmail.com>
2022-10-01 02:39:16 -0300

    Implement rotate curve

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,20 +1,43 @@
+using System.Collections;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
     [SerializeField] private Transform playerTransform = default;
     [SerializeField] private Vector3 cameraOffset;
     [SerializeField] private float smoothTime = 0.3F;
+    
+    [SerializeField] private AnimationCurve rotateCurve = null;
+    [SerializeField] private float rotateDelay = 0f;
+
     private Vector3 velocity = Vector3.zero;
 
     private void LateUpdate()
     {
-        if(playerTransform != null) 
+        if (playerTransform != null) 
         {
             Vector3 targetPosition = playerTransform.position + cameraOffset;
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         }
     }
 
+    public void Rotate(Vector3 rotation)
+    {
+        StartCoroutine(RotateDelay(transform.eulerAngles, rotation));
+    }
+
+    private IEnumerator RotateDelay(Vector3 origin, Vector3 target)
+    {
+        float timer = 0f;
+        while (timer < rotateDelay)
+        {
+            timer += Time.deltaTime;
+            float lerp = rotateCurve.Evaluate(timer / rotateDelay);
+
+            transform.eulerAngles = Vector3.Lerp(origin, target, lerp);
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.eulerAngles = target;
+    }
 }

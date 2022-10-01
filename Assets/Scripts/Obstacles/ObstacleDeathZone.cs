c23e6c0c33921ca0781c519ab7ddcc779df562ba
Adrian Sgro <Owinmowe@gmail.com>
2022-10-01 20:00:00 -0300

    Create Death zone obstacle prefab

---
+++ Assets/Scripts/Obstacles/ObstacleDeathZone.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class ObstacleDeathZone : MonoBehaviour, IObstacle
+{
+    private ChildrenCollision[] childrenCollision;
+    private void Start ()
+    {
+        childrenCollision = GetComponentsInChildren<ChildrenCollision>();
+        for (int i = 0; i < childrenCollision.Length; i++)
+        {
+            childrenCollision[i].OnHit += CheckIsPlayer;
+        }
+    }
+
+    private void OnCollisionEnter (Collision other)
+    {
+        CheckIsPlayer(other.transform);
+    }
+
+    public void CheckIsPlayer (Transform other)
+    {
+        if (Utils.CheckLayerInMask(GameplayManager.Get().layerPlayer, other.gameObject.layer))
+        {
+            IDamageable damageable = other.transform.GetComponent<IDamageable>();
+            if (damageable != null)
+                Kill(damageable);
+        }
+    }
+
+    public void Kill (IDamageable damageable)
+    {
+        damageable.Kill();
+    }
+}

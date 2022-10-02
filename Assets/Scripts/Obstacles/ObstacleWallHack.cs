e2387d5dea9bbe5aea30739d752760aaf29879ac
Adrian Sgro <Owinmowe@gmail.com>
2022-10-01 22:28:35 -0300

    Add Wall Hack Obstacle prefab to the project

---
+++ Assets/Scripts/Obstacles/ObstacleWallHack.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class ObstacleWallHack : MonoBehaviour, IObstacle
+{
+
+    [SerializeField] private WallType wallType = default;
+    [SerializeField] private float wallForwardSpeed = 5f;
+    [SerializeField] private float wallBackwardSpeed = 1f;
+    [SerializeField] private Transform wallEndTransform = default;
+
+    [Header("Timing Wall Configuration")]
+    [SerializeField] private float wallWaitTime = 2f;
+
+    [Header("Player In Front Wall Configuration")] 
+    [SerializeField] private LayerMask playerLayerMask = default;
+    [SerializeField] private float wallFrontRaycastDistance = 10f;
+    
+    private Vector3 startPosition = Vector3.zero;
+    private Vector3 endPosition = Vector3.zero;
+    private ChildrenCollision[] childrenCollision = default;
+
+    public enum  WallType
+    {
+        Timing,
+        OnPlayerInFront
+    }
+    
+    private void Start ()
+    {
+        childrenCollision = GetComponentsInChildren<ChildrenCollision>();
+        for (int i = 0; i < childrenCollision.Length; i++)
+        {
+            childrenCollision[i].OnHit += CheckIsPlayer;
+        }
+
+        startPosition = transform.position;
+        endPosition = wallEndTransform.position;
+        
+        switch (wallType)
+        {
+            case WallType.Timing:
+                StartCoroutine(TimingWallCoroutine());
+                break;
+            case WallType.OnPlayerInFront:
+                StartCoroutine(InFrontPlayerWallCoroutine());
+                break;
+        }
+    }
+
+    public void CheckIsPlayer (Transform other)
+    {
+        if (Utils.CheckLayerInMask(GameplayManager.Get().layerPlayer, other.gameObject.layer))
+        {
+            IDamageable damageable = other.transform.GetComponent<IDamageable>();
+            if (damageable != null)
+                Kill(damageable);
+        }
+    }
+
+    public void Kill (IDamageable damageable)
+    {
+        damageable.Kill();
+    }
+
+    private IEnumerator InFrontPlayerWallCoroutine()
+    {
+        while (enabled)
+        {
+            yield return null;
+            if (Physics.Raycast(transform.position, transform.right, wallFrontRaycastDistance, playerLayerMask))
+            {
+                float t = 0;
+                while (t < 1)
+                {
+                    t += Time.deltaTime * wallForwardSpeed;
+                    transform.position = Vector3.Lerp(startPosition, endPosition, t);
+                    yield return null;
+                }
+
+                transform.position = wallEndTransform.position;
+            
+                while (t > 0)
+                {
+                    t -= Time.deltaTime * wallBackwardSpeed;
+                    transform.position = Vector3.Lerp(startPosition, endPosition, t);
+                    yield return null;
+                }
+
+                transform.position = startPosition;
+            }
+        }
+    }
+
+    private IEnumerator TimingWallCoroutine()
+    {
+        while (enabled)
+        {
+            yield return new WaitForSeconds(wallWaitTime);
+            float t = 0;
+            while (t < 1)
+            {
+                t += Time.deltaTime * wallForwardSpeed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, t);
+                yield return null;
+            }
+
+            transform.position = wallEndTransform.position;
+            
+            while (t > 0)
+            {
+                t -= Time.deltaTime * wallBackwardSpeed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, t);
+                yield return null;
+            }
+
+            transform.position = startPosition;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + transform.right * wallFrontRaycastDistance);
+    }
+}

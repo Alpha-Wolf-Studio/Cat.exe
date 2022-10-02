b6d5cde1fe782a09acf05bed802d3cdbdbca9c92
Adrian Sgro <Owinmowe@gmail.com>
2022-10-02 10:50:09 -0300

    Change leaderboard UI script to better reflect its use

--- Assets/_External Dependencies/Playfab Test/LeaderboardUI.cs
+++ Assets/_External Dependencies/Playfab Test/LeaderboardUI.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CreditsPanelUI : MonoBehaviour
+public class LeaderboardUI : MonoBehaviour
 {
 
     [Header("Leaderboard")] 

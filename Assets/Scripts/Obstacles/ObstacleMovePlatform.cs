a5a328e0a9a6e45680acc4c78b87f4ffeb7355dd
micalbenitez <benitezmicaelaluz@gmail.com>
2022-10-03 17:52:52 -0300

    Improve move platform obstacles

--- Assets/Scripts/Obstacles/ObstacleMovePlatform.cs
+++ Assets/Scripts/Obstacles/ObstacleMovePlatform.cs
@@ -5,18 +5,16 @@ public class ObstacleMovePlatform : MonoBehaviour
 {
 
     [SerializeField] private UnityEvent OnSpawn = default;
-    
+
     [Header("Move platform")]
+    [SerializeField] private GameObject model = default;
     [SerializeField] private bool rightDirection = false;
     [SerializeField] private float speed = 0;
-    [SerializeField] private int rightBound = 0;
-    [SerializeField] private int leftBound = 0;
-
-    private float initialXPosition = 0;
+    [SerializeField] private float rightBound = 0;
+    [SerializeField] private float leftBound = 0;
 
     private void Start()
     {
-        initialXPosition = transform.position.x;
         OnSpawn?.Invoke();
     }
 
@@ -24,13 +22,13 @@ public class ObstacleMovePlatform : MonoBehaviour
     {
         if (rightDirection)
         {
-            transform.Translate(Vector2.right * speed * Time.deltaTime);
-            if (transform.position.x >= (initialXPosition + rightBound)) rightDirection = false;
+            model.transform.localPosition += Vector3.right * speed * Time.deltaTime;
+            if (model.transform.localPosition.x > rightBound) rightDirection = false;
         }
         else
         {
-            transform.Translate(Vector2.right * -speed * Time.deltaTime);
-            if (transform.position.x <= (initialXPosition + leftBound)) rightDirection = true;
+            model.transform.localPosition += Vector3.left * speed * Time.deltaTime;
+            if (model.transform.localPosition.x < leftBound) rightDirection = true;
         }
     }
 }

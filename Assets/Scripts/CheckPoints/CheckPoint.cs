d08878b37129399dd3dc497368accaf6bedd9228
Adrian Sgro <Owinmowe@gmail.com>
2022-10-03 20:50:20 -0300

    Add glow to the checkpoint

--- Assets/Scripts/CheckPoints/CheckPoint.cs
+++ Assets/Scripts/CheckPoints/CheckPoint.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
@@ -10,10 +11,21 @@ public class CheckPoint : MonoBehaviour
     [SerializeField] private Collider colliderTrigger;
     [SerializeField] private Collider wall;
     [SerializeField] private Transform spawnPoint;
-
+    [Header("Graphic")]
+    [SerializeField] private MeshRenderer graphicRenderer;
+    [SerializeField] private float glowSpeed = 1f;
+    [SerializeField] private Color glowColor; 
+    [SerializeField] private Color blackColor;
+    
     public bool wasActivated;
+    private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
 
     public int ID { get; set; } = 0;
+
+    private void Start()
+    {
+        StartCoroutine(GlowIEnumerator());
+    }
 
     private void OnTriggerEnter (Collider other)
     {
@@ -45,4 +57,30 @@ public class CheckPoint : MonoBehaviour
 
     public Vector3 GetPositionSpawn () => spawnPoint.position;
     public Vector3 GetRotationSpawn () => spawnPoint.rotation.eulerAngles;
+
+    private IEnumerator GlowIEnumerator()
+    {
+        while (enabled)
+        {
+            float t = 0;
+            while (t < 1)
+            {
+                t += Time.deltaTime * glowSpeed;
+                Color color = Color.Lerp(blackColor, glowColor, t); 
+                graphicRenderer.material.SetColor(EmissionColor, color);
+                yield return null;
+            }
+            graphicRenderer.material.SetColor(EmissionColor, glowColor);
+            t = 1;
+            while (t > 0)
+            {
+                t -= Time.deltaTime * glowSpeed;
+                Color color = Color.Lerp(blackColor, glowColor, t); 
+                graphicRenderer.material.SetColor(EmissionColor, color);
+                yield return null;
+            }
+            graphicRenderer.material.SetColor(EmissionColor, blackColor);
+        }
+    }
+    
 }

3d6d2f8bd61581667ab0a11b93361f82df45b966
Adrian Sgro <Owinmowe@gmail.com>
2022-10-03 03:44:36 -0300

    Fix spotlight merge conflict

--- Assets/Scripts/Player/ShadowUnderPlayer.cs
+++ Assets/Scripts/Player/ShadowUnderPlayer.cs
@@ -3,34 +3,23 @@ using UnityEngine;
 
 public class ShadowUnderPlayer : MonoBehaviour
 {
+    private Transform target;
+    private float startY;
+    [SerializeField] private Light[] spotlights;
+    [SerializeField] private float maxSpotAngle = 30;
+    [SerializeField] private float multiplyFactorScale = 1.5f;
 
-
-
-    //private Light spotlight;
-    //private Transform target;
-    //private float startY;
-    //[SerializeField] private float maxSpotAngle = 30;
-    //[SerializeField] private float multiplyFactorScale = 1.5f;
-    //
-    //void Start ()
-    //{
-    //    target = GameplayManager.Get().playerController.transform;
-    //    startPosition = transform.position;
-    //
-    //private void Awake ()
-    //{
-    //    spotlight = GetComponent<Light>();
-    //}
-    //
-    //void Start ()
-    //{
-    //    target = GameplayManager.Get().playerController.transform;
-    //    startY = target.position.y;
-    //
-    //}
-    //
-    //void Update ()
-    //{
-    //    spotlight.spotAngle = (startY / (target.position.y * multiplyFactorScale)) * maxSpotAngle;
-    //}
+    void Start ()
+    {
+        target = GameplayManager.Get().playerController.transform;
+        startY = target.position.y;
+    }
+    
+    void Update ()
+    {
+        foreach (var spotlight in spotlights)
+        {
+            spotlight.spotAngle = (startY / (target.position.y * multiplyFactorScale)) * maxSpotAngle;
+        }
+    }
 }
